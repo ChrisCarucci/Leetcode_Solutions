75ae23365016cfa62c5d4a15a0a791dd62044dd1
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-02-07 22:44:20 -0330

    In Order Successor BST Solution

---
+++ Medium/InOrderSuccessorBST.js
@@ -0,0 +1,28 @@
+
+
+const inOrderSuccessor = function(root, p) {
+
+    if(p.right) {
+        let cur = p.right;
+        let prev = null;
+        while(cur) {
+            prev = cur;
+            cur = cur.left;
+        }
+
+        return prev;
+    }
+
+    let cur = root;
+    let prev = null;
+
+    while(cur !== p) {
+        if(p.val < cur.val) {
+            prev = cur;
+            cur = cur.left;
+        } else {
+            cur = cur.right;
+        }
+    }
+    return prev;
+}
