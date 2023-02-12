2a6f7f0e9a84e575c1c755dc9c50486486ee35d0
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-02-11 23:59:22 -0330

    Permutation second solutioon

--- Medium/permutations.js
+++ Medium/permutations.js
@@ -12,3 +12,17 @@ const permute = function(nums) {
         }, [])
     }, [[]])
 };
+
+
+const permute = function(nums, arr = [], res = []) {
+
+    if (nums.length === 0) res.push([...arr]);
+
+    for (let i = 0; i < nums.length; i++) {
+        let rest = nums.filter((n, index) => index !== i);
+        arr.push(nums[i]);
+        permute(rest, arr, res);
+        arr.pop()
+    }
+    return res
+}
