95e2bc13b1c201f7d998ba289c6601176c3d4265
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-02-11 23:12:35 -0330

    Combinations Solution

---
+++ Medium/Combinations.js
@@ -0,0 +1,16 @@
+const combine = function(n, k) {
+    let result = [];
+    
+    function dfs(current, start) {
+        if(current.length == k) {
+            result.push(current);
+            return;
+        }
+        for(let i = start; i <= n; i++) {
+            dfs([...current, i], i + 1);
+        }
+    }
+    
+    dfs([], 1);
+    return result;
+};
