264883d6b7d53a98798b0da267e31261bb3a6744
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-03-03 23:23:29 -0330

    Recursive Solution Added to Stack Solution

--- Easy/BTPostOrderTraversal.js
+++ Easy/BTPostOrderTraversal.js
@@ -12,3 +12,21 @@ const postorderTraversal = function(root) {
     
     return result.reverse();
 };
+
+
+// Recursive Solution
+
+
+const postorderTraversal = function(root) {
+    const result = [];
+    const recursive = (node) => {
+        if(!node) return;
+
+        if(node.left) recursive(node.left);
+        if(node.right) recursive(node.right);
+        result.push(node.val);
+    }
+
+    recursive(root);
+    return result;
+};
